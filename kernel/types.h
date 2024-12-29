6f4f43dd00339172ea09d5f3d7fe90c006cf5ebc
hxc314159 <146416839+hxc314159@users.noreply.github.com>
2024-12-30 00:06:07 +0800

    Update types.h

--- kernel/types.h
+++ kernel/types.h
@@ -8,3 +8,13 @@ typedef unsigned int  uint32;
 typedef unsigned long uint64;
 
 typedef uint64 pde_t;
+
+
+// typedef unsigned int uint; 将 unsigned int 类型重命名为 uint。
+// typedef unsigned short ushort; 将 unsigned short 类型重命名为 ushort。
+// typedef unsigned char uchar; 将 unsigned char 类型重命名为 uchar。
+// typedef unsigned char uint8; 将 unsigned char 类型重命名为 uint8，表示一个 8 位无符号整数。
+// typedef unsigned short uint16; 将 unsigned short 类型重命名为 uint16，表示一个 16 位无符号整数。
+// typedef unsigned int uint32; 将 unsigned int 类型重命名为 uint32，表示一个 32 位无符号整数。
+// typedef unsigned long uint64; 将 unsigned long 类型重命名为 uint64，表示一个 64 位无符号整数。
+// typedef uint64 pde_t; 将 uint64 类型重命名为 pde_t，表示一个页面目录项的类型。这个在操作系统开发中常见，表示一个指向页目录的 64 位地址。
